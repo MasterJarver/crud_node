4623011ec96a0f5e1f9c49f47da9b7991ac2046d
jarver <masterlogdog@gmail.com>
2019-02-10 01:33:02 +0200

    install ejs and express-ejs-layouts packages

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,7 +1,7 @@
 // main controller file, в контроллере описан функционал, котрый вызывается из роутера
-module.exports = {
+module.exports = { // экспорт объекта с полями
     // show the home page
-    showHome: (req, res) => {
+    showHome: (req, res) => { // метод showHome
         res.send('Hello, I am the app!');
     }
 };
