d2ae5d8847ce4bb6cf1849a898548883f2039cbe
jarver <masterlogdog@gmail.com>
2019-02-10 23:21:19 +0200

    show events from database in /events

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -4,34 +4,41 @@ module.exports = { // экспрт функии получения даннх и
     showSingle : showSingle,
     seedEvents : seedEvents
 };
-    // show all events
-    function showEvents(req,res) { // функция показа
-        // get all events
+// show all events
+function showEvents(req,res) { // функция показа
+    // get all events
+    Event.find({}, (err, events) => { // поиск всех записей в коллекции
+        if(err) {
+            res.status(404);
+            res.send('Events not found');
+        }
         //return a view with data
         res.render('pages/events', {events: events}); // отображение по маршруту массива events
-    }
-    // show a single event
-    function showSingle(req, res) {
-        // get a single event
-        const event = {name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.'};
-        res.render('pages/single', {event: event});
-    }
-    // seed our database
-    function seedEvents(req, res) {
-        // create some events
-        const events = [
-            {name: 'Basketball', description: 'Throwing into a basket.'},
-            {name: 'Swimming', description: 'Travelling distance by water.'},
-            {name: 'Weightlifting', description: 'Lifting heavy things up.'},
-            {name: 'Ping Pong', description: 'Super fast paddles'}
-        ];
-        // use the Event model to insert/save
-        Event.remove({}, () => { // удаление всех записей в коллекции
-            for(event of events) { // итерация по массиву events
-                let newEvent = new Event(event); // создание экземпляра event
-                newEvent.save().catch(err => console.log(err)); // сохранение в бд ивента
-            }
-        });
-        // seeded!
-        res.send('Database seeded!');
-    }
+    });
+
+}
+// show a single event
+function showSingle(req, res) {
+    // get a single event
+    const event = {name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.'};
+    res.render('pages/single', {event: event});
+}
+// seed our database
+function seedEvents(req, res) {
+    // create some events
+    const events = [
+        {name: 'Basketball', description: 'Throwing into a basket.'},
+        {name: 'Swimming', description: 'Travelling distance by water.'},
+        {name: 'Weightlifting', description: 'Lifting heavy things up.'},
+        {name: 'Ping Pong', description: 'Super fast paddles'}
+    ];
+    // use the Event model to insert/save
+    Event.remove({}, () => { // удаление всех записей в коллекции
+        for(event of events) { // итерация по массиву events
+            let newEvent = new Event(event); // создание экземпляра event
+            newEvent.save().catch(err => console.log(err)); // сохранение в бд ивента
+        }
+    });
+    // seeded!
+    res.send('Database seeded!');
+}
