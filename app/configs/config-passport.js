fc082dd4633eb452130477c476e8c26ab6a63138
Jarver <masterlogdog@gmail.com>
2019-02-21 12:52:20 +0200

    add fake user and serialize/deserialize

---
+++ app/configs/config-passport.js
@@ -0,0 +1,19 @@
+/* SETUP_PASSPORT */
+const passport = require('passport'); // модуль для работы с аутентификацией и авторизацией
+const LocalStrategy = require('passport-local').Strategy; // выбор стратегии аутентификации и авторизации
+const userDB = { // фейковый пользователь
+    id: 1,
+    email: 'masterjarver@ukr.net', // логин
+    password: '123' // пароль в явном виде(вообще пароль всегда нужно шифровать)
+};
+// serialize
+passport.serializeUser((user, done) => {
+    console.log('Serialize: ', user);
+    done(null, user.id); // сохранение в сессию пользовательского id
+});
+// deserialize
+passport.deserializeUser((id, done) => {
+    console.log('Deserialize: ', id); // 1
+    const user = (userDB.id === id) ? userDB : false;
+    done(null, user); // сохранение в сессию пользовательского id
+});
